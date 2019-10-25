f27a91404e8a11bc9a8a1fb3da34f4880ebee6b6
Ire Aderinokun <iaderinokun@gmail.com>
2019-10-25 16:25:42 -0700

    fixes

--- public/script.js
+++ public/script.js
@@ -28,6 +28,9 @@ document.querySelector('button[type="submit"]').addEventListener('click', (e) =>
     fetch("/.netlify/functions/take-screenshot", options)
         .then((res) => res.json())
         .then((res) => {
+            
+            if (!res.buffer) return document.getElementById('result').textContent = 'Error capturing screenshot';
+
             const img = document.createElement('img');
             img.src = bufferToImageUrl(res.buffer.data);
             document.getElementById('result').innerHTML = img.outerHTML;
